aa4dd1371b5c4aa7e72c9fdc2bd053a0122def38
Furkan Yamak <furkan61400@gmail.com>
2024-05-06 20:59:17 +0300

    initialization

--- notionapp/asgi.py
+++ notionapp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for notionFM project.
+ASGI config for notionapp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notionFM.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notionapp.settings')
 
 application = get_asgi_application()
