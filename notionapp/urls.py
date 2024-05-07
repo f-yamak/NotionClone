6a549a2dc08cfb378f8515bc38a6bb0d22fdae8f
Furkan Yamak <furkan61400@gmail.com>
2024-05-07 13:25:00 +0300

    Update | static file settings

--- notionapp/urls.py
+++ notionapp/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('',include("block.urls")),
     path('account/',include("account.urls")),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
